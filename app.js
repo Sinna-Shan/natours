c10e250c137bd50e506b56ca8f3568f34ade290a
Noah Shan <lust595@gmail.com>
2023-12-14 22:48:59 +0530

    update a tour

--- app.js
+++ app.js
@@ -17,22 +17,22 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
-app.get('/api/v1/tours/:id',(req,res)=>{
-    const id = req.params.id * 1;
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
 
-    if(id > tours.length){
-        return res.status(404).json({
-            status: 'failed',
-            message: 'Invalid ID provided!'
-        })
-    }
-    const tour = tours.find(el=>el.id === id)
-    res.status(200).json({
-        status:'success',
-        data: {
-            tour
-        }
-    })
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID provided!',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
 });
 
 app.post('/api/v1/tours', (req, res) => {
@@ -46,6 +46,25 @@ app.post('/api/v1/tours', (req, res) => {
       res.status(201).json({ status: 'success', data: { tour: newTour } });
     }
   );
+});
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID provided!',
+    });
+  }
+  const tour = tours.find((el) => el.id === id);
+  tours.push(tour);
+  res.send({
+    status: 'success',
+    data: {
+      tour,
+    }
+  });
 });
 
 const port = 3000;
