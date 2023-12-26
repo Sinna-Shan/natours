3f158ce079e4e06ea91c5a508c31333fdf40f6bb
Noah Shan <lust595@gmail.com>
2023-12-26 10:09:09 +0530

    sorting added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,10 +11,16 @@ exports.getAllTours = async (req, res) => {
     // ADVANCE FILTERING
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
+    // SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
     // EXECUTE QUERY
     const tours = await query;
 
