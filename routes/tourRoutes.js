978abf9825d427bfbf2e968cb48cc123c388caa3
Noah Shan <lust595@gmail.com>
2023-12-29 22:15:31 +0530

    refactored API features

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,11 @@ const {
   getTourById,
   updateTour,
   deleteTour,
+  aliasTopTours
 } = require('../controllers/tourController');
 
 const router = express.Router();
-
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTourById).patch(updateTour).delete(deleteTour);
 
